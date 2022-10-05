a43341e8e29bc82d3c986f1e42de88ce80102c25
Magib <112185392+mageeeb@users.noreply.github.com>
2022-10-05 11:49:34 +0200

    lesson php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// commentaire sur une ligne
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+
+<body>
+    
+    <?php
+    echo "<h1>hello world</h1>";
+    ?>
+</body>
+
+</html>
