6c7d5961f0980f765624fda09966bc6c76eaaf58
Magib <112185392+mageeeb@users.noreply.github.com>
2022-10-05 12:17:28 +0200

    sauv PHP

---
+++ comment.php
@@ -0,0 +1,10 @@
+<?php
+
+//echo "<h1>coucou sava</h1>";
+?>
+
+<?php
+
+
+echo "<p>Temps Unix de la page</p>";
+?>
--- test.php
+++ test.php
@@ -1,21 +1,19 @@
 <?php
-// commentaire sur une ligne
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Document</title>
 </head>
 
 <body>
-    
     <?php
-    echo "<h1>hello world</h1>";
+    echo "<h1>Hello world</h1>";
     ?>
 </body>
 
